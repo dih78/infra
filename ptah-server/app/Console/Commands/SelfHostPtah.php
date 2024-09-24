fcb55c00bb4471e33f70896f774149a207c584ea
dih78 <khadjiradev@gmail.com>
2024-09-24 12:31:20 +0200

    change image

--- ptah-server/app/Console/Commands/SelfHostPtah.php
+++ ptah-server/app/Console/Commands/SelfHostPtah.php
@@ -216,7 +216,7 @@ class SelfHostPtah extends Command
                     'name' => 'app',
                     'launchMode' => LaunchMode::Daemon->value,
                     'dockerRegistryId' => null,
-                    'dockerImage' => 'InfraImg',
+                    'dockerImage' => 'infraimg',
                     'releaseCommand' => [
                         'command' => 'php artisan config:cache && php artisan migrate --no-interaction --verbose --ansi --force',
                     ],
